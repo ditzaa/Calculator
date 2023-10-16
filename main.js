b56479c82fe9d27af13580c78e570bd8e9136ce1
Mitu Adrian <adrianmitu83@gmail.com>
2023-10-16 11:52:53 +0300

    added calculatordisplay and buttons

--- main.js
+++ main.js
@@ -26,3 +26,33 @@ let operator;
 function operate(firstNumber, secondNumber, operator){
     return add(firstNumber, secondNumber);
 }
+
+const mainContainer = document.querySelector('#calculator-div');
+const buttonsContainer = document.querySelector('#buttons-container');
+
+// const rowButtonsContainer = document.createElement('div');
+//     rowButtonsContainer.classList.add('row-container');
+//     buttonsContainer.appendChild(rowButtonsContainer);
+
+//buttons are ordered left to right, bottom to top 
+let idOfButton = 'button0';
+index = 0;
+
+for(i=0; i<4; i++){
+    const rowButtonsContainer = document.createElement('div');
+    rowButtonsContainer.classList.add('row-container');
+    buttonsContainer.appendChild(rowButtonsContainer);
+
+    
+    for(j=0; j<4; j++){
+        const calculatorButton = document.createElement('div');
+        calculatorButton.classList.add('calculator-button');
+        calculatorButton.setAttribute('id', idOfButton);
+        index++;
+        idOfButton = 'button' + index.toString();
+        rowButtonsContainer.appendChild(calculatorButton);
+    }
+}
+
+//add text to buttons
+let textToButtons = '0'
